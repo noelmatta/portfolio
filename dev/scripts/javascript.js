21091d456143f774966f66bc645bf43636654f1f
Noel Matta <noelmatta82@gmail.com>
2018-03-11 20:54:15 -0400

    portfolio navigation done

--- dev/scripts/javascript.js
+++ dev/scripts/javascript.js
@@ -12,44 +12,20 @@ $("main").onepage_scroll({
   direction: "vertical"
 });
 
+const portfolio = {};
 
-$(document).ready(function () {
+portfolio.pieceToggle = () => {
+  $('.portfolioBlock').on('click', function() {
+    let selectedPiece = $(this).attr('name');
+    $('.piece').hide();
+    $(`#${selectedPiece}`).show();
+  })
+}
 
-  $('.tabs_default').tabslet();
+portfolio.events = () => {
+  portfolio.pieceToggle();
+}
 
-  $('.tabs_active').tabslet({
-    active: 2
-  });
-
-  $('.tabs_hover').tabslet({
-    mouseevent: 'hover',
-    attribute: 'href',
-    animation: false
-  });
-
-  $('.tabs_animate').tabslet({
-    mouseevent: 'click',
-    attribute: 'href',
-    animation: true
-  });
-
-  $('.tabs_rotate').tabslet({
-    autorotate: true,
-    delay: 3000
-  });
-
-  $('.tabs_controls').tabslet();
-
-  $('.before_event').tabslet();
-  $('.before_event').on("_before", function () {
-    alert('This alert comes before the tab change!')
-  });
-
-  $('.after_event').tabslet({
-    animation: true
-  });
-  $('.after_event').on("_after", function () {
-    alert('This alert comes after the tab change!')
-  });
-
+$(function () {
+  portfolio.events();
 });
