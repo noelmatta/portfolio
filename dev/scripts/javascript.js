2bf99d67aa38fc2997e84f0d8574f571c8576742
Noel Matta <noelmatta82@gmail.com>
2018-03-12 22:03:47 -0400

    2 breakpoints added

--- dev/scripts/javascript.js
+++ dev/scripts/javascript.js
@@ -15,6 +15,16 @@ $("main").onepage_scroll({
 
 const portfolio = {};
 
+portfolio.clearInput = () => {
+  $('input').on('focus', function() {
+    $(this).val('');
+  });
+  
+  $('textarea').on('focus', function () {
+    $(this).val('');
+  });
+}
+
 portfolio.pieceToggle = () => {
   $('.portfolioBlock').on('click', function() {
     let selectedPiece = $(this).attr('name');
@@ -25,6 +35,7 @@ portfolio.pieceToggle = () => {
 
 portfolio.events = () => {
   portfolio.pieceToggle();
+  portfolio.clearInput();
 }
 
 $(function () {
